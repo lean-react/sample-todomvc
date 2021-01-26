5184d551f032ded5bbbfb79ad5542a31e149a7ba
Michael Alt <mail@lean-stack.de>
2021-01-26 23:05:45 +0100

    Clean up scaffolded code.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders todos heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appHeading = screen.getByRole('heading',{ name: /todos/i });
+  expect(appHeading).toBeInTheDocument();
 });
