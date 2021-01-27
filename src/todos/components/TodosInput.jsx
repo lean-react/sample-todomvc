866ef7e1db62e22a14deceaa23a819b5137626a8
Michael Alt <mail@lean-stack.de>
2021-01-27 08:01:05 +0100

    Implement TodosInput with an uncontrolled input.

--- src/todos/components/TodosInput.jsx
+++ src/todos/components/TodosInput.jsx
@@ -1,6 +1,18 @@
-export function TodosInput() {
+export function TodosInput({onCreateTodo}) {
+
+  function handleEnter(ev) {
+    if (ev.which === 13) {
+      const title = ev.target.value.trim();
+      if (title) {
+        onCreateTodo(title);
+      }
+      ev.target.value = '';
+    }
+  }
+
   return (
     <input
+      onKeyUp={handleEnter}
       className="new-todo"
       placeholder="What needs to be done?"
       autoFocus
