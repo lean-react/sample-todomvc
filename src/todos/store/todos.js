7d245d5d91545c9567539b13665138f7cf8853c7
Micha Alt <mail@lean-stack.de>
2021-01-29 12:45:47 +0100

    Test extra reducers.

--- src/todos/store/todos.js
+++ src/todos/store/todos.js
@@ -1,6 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from '../api/local-backend';
 
+export const loadTodos = createAsyncThunk( 'todos/load',
+  () => api.getAll()
+);
+
 export const createTodo = createAsyncThunk( 'todos/createTodo',
   (title) => api.create(title)
 );
@@ -10,8 +14,16 @@ const todosSlice = createSlice({
   initialState: {
     items: []
   },
-  reducers: {},
-  extraReducers: {}
+  reducers: {
+  },
+  extraReducers: {
+    [loadTodos.fulfilled]: (state, action) => {
+      state.items = action.payload;
+    },
+    [createTodo.fulfilled]: (state, action) => {
+      state.items.push(action.payload);
+    }
+  }
 });
 
 export default todosSlice.reducer;
--- src/todos/store/todos.test.js
+++ src/todos/store/todos.test.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import api from '../api/local-backend';
-import todos, { createTodo } from './todos';
+import todos, { createTodo, loadTodos } from './todos';
 
 // Mock backend module
 jest.mock('../api/local-backend');
@@ -16,5 +16,26 @@ describe('Todos Slice', () => {
     const title = 'Test an async thunk action';
     store.dispatch(createTodo(title));
     expect(api.create).toHaveBeenCalledWith(title);
+  });
+
+  test('createTodo fulfilled adds created todo', () => {
+    const initialState = { items: [] };
+    const todo = { id: 17, title: 'Extra Reducer testing', completed: true };
+    const action = { type: createTodo.fulfilled.type, payload: todo };
+    const state = todos(initialState, action);
+    expect(state.items[0]).toBe(todo);
+  });
+
+  test('loadTodos action makes an api call', () => {
+    store.dispatch(loadTodos());
+    expect(api.getAll).toHaveBeenCalled();
+  });
+
+  test('loadTodos fulfilled sets the state items', () => {
+    const initialState = { items: [] };
+    const items = [1,2,3];
+    const action = { type: loadTodos.fulfilled.type, payload: items };
+    const state = todos(initialState, action);
+    expect(state.items).toBe(items);
   });
 });
