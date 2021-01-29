721b546f0f3d5b1dd07ead263fab3b7c6110370a
Micha Alt <mail@lean-stack.de>
2021-01-29 13:28:36 +0100

    Test rendering todo items in list.

--- src/todos/components/TodosList.test.js
+++ src/todos/components/TodosList.test.js
@@ -1,10 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { TodosList } from './TodosList';
-import store from '../store';
+
+import configureStore from 'redux-mock-store';
+
 
 describe('TodosList component', () => {
   test('renders zero todos with empty state', () => {
+    const store = configureStore([])({ todos: { items: [] }});
+
     render(
       <Provider  store={store}>
         <TodosList />
@@ -12,5 +16,20 @@ describe('TodosList component', () => {
     );
 
     expect(screen.queryByRole('listitem')).not.toBeInTheDocument();
+  });
+
+  test('renders x todos with x items in state', () => {
+    const store = configureStore([])({ todos: { items: [
+      { id: 1, title: 'Todo 1', completed: true },
+      { id: 2, title: 'Todo 2', completed: false },
+    ] }});
+
+    render(
+      <Provider  store={store}>
+        <TodosList />
+      </Provider>
+    );
+
+    expect(screen.queryAllByRole('listitem').length).toBe(2);
   });
 });
