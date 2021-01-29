9929b1be5b8fb71db3296feee19e570a880e2697
Michael Alt <mail@lean-stack.de>
2021-01-29 23:21:24 +0100

    Show off bug spotted with e2e suite.

--- src/todos/components/TodosItem.test.js
+++ src/todos/components/TodosItem.test.js
@@ -1,36 +1,46 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { TodosItem } from './TodosItem';
+import userEvent from '@testing-library/user-event';
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import * as actions from '../store/todos';
 
 describe('TodosItem component', () => {
 
   let todo;
+  let store;
 
   beforeEach(() => {
     todo = { id: 42, title: 'Simple Tests at component level', completed: true };
+    const mockStore = configureStore([thunk]);
+    store = mockStore({ todos: { items: [] }});
   });
 
   describe('view mode', () => {
 
     test('displays the todo title', () => {
-      render(<TodosItem todo={todo}></TodosItem>);
+      render(<Provider store={store}><TodosItem todo={todo}></TodosItem></Provider>);
       expect(screen.getByText(todo.title)).toBeInTheDocument();
     });
 
     test('displays the completed state', () => {
-      const { rerender } = render(<TodosItem todo={todo}></TodosItem>);
+      const { rerender } = render(<Provider store={store}><TodosItem todo={todo}></TodosItem></Provider>);
       expect(screen.getByRole('checkbox', {checked: true})).toBeInTheDocument();
 
       todo = { ...todo, completed: !todo.completed };
-      rerender(<TodosItem todo={todo}></TodosItem>);
+      rerender(<Provider store={store}><TodosItem todo={todo}></TodosItem></Provider>);
       expect(screen.getByRole('checkbox', {checked: false})).toBeInTheDocument();
     });
 
     test('renders the completed class at the li-item', () => {
-      const { rerender } = render(<TodosItem todo={todo}></TodosItem>);
+      const { rerender } = render(<Provider store={store}><TodosItem todo={todo}></TodosItem></Provider>);
       expect(screen.getByRole('listitem')).toHaveClass('completed');
 
       todo = { ...todo, completed: !todo.completed };
-      rerender(<TodosItem todo={todo}></TodosItem>);
+      rerender(<Provider store={store}><TodosItem todo={todo}></TodosItem></Provider>);
       expect(screen.getByRole('listitem')).not.toHaveClass('completed');
     });
 
@@ -39,17 +49,27 @@ describe('TodosItem component', () => {
   describe('edit mode', () => {
 
     test('is entered with dblclick at label', () => {
-      render(<TodosItem todo={todo}></TodosItem>);
+      render(<Provider store={store}><TodosItem todo={todo}></TodosItem></Provider>);
       fireEvent.dblClick(screen.getByText(todo.title));
       const editFld = screen.getByRole('textbox');
       expect(editFld).toBeInTheDocument();
     });
 
     test('shows initially original title', () => {
-      render(<TodosItem todo={todo}></TodosItem>);
+      render(<Provider store={store}><TodosItem todo={todo}></TodosItem></Provider>);
       fireEvent.dblClick(screen.getByText(todo.title));
       const editFld = screen.getByRole('textbox');
       expect(editFld.value).toBe(todo.title);
+    });
+
+    test('trims the entered title', async () => {
+      jest.spyOn(actions, 'updateTodoTitle');
+
+      render(<Provider store={store}><TodosItem todo={todo}></TodosItem></Provider>);
+      fireEvent.dblClick(screen.getByText(todo.title));
+      userEvent.type(screen.getByRole('textbox'), '{selectall}{backspace}   Trimmed  {enter}');
+
+      expect(actions.updateTodoTitle).toHaveBeenCalledWith({ id: 42, title: 'Trimmed' });
     });
   });
 });
